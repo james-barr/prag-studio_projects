89468b9f34cf48bb02d38fac9b2fc3ef68b107ec
James <jamesbarr711@gmail.com>
2017-08-21 16:45:02 -0600

    add signin authentication for username and email

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,12 @@ class SessionsController < ApplicationController
   end
 
   def create
-    params[:email]
-    params[:password]
+    if user = User.authenticate(params[:email], params[:password])
+      flash[:notice] = "Welcome back, #{user.name}"
+      redirect_to user_path(user)
+    else
+      flash.now[:alert] = "Error, invalid login credentials"
+      render 'new'
+    end
   end
 end
--- spec/features/sessions/new_session_spec.rb
+++ spec/features/sessions/new_session_spec.rb
@@ -6,9 +6,50 @@ describe "New session creation: " do
     visit signin_path
     e(page).to have_field "Email"
     e(page).to have_field "Password"
+    e(page).to have_selector "input[type='submit']"
     e(page).to have_xpath "//input[@required='required']"
     e(page).to have_link "Sign up"
     e(page).to have_selector "input[type=submit]"
+  end
+
+  it "does not authenticate without a password & flashes an error" do
+    u = User.create user_attributes email: "x@x", password: "y", password_confirmation: "y"
+    visit signin_path
+    fill_in "Email or Username", with: "x@x"
+    fill_in "Password", with: ""
+    click_button "Sign In"
+    e(current_path).to eq session_path
+    e(page).to have_text "Error"
+  end
+
+  it "does not authenticate without a username / email & flashes an error" do
+    u = User.create user_attributes email: "x@x", password: "y", password_confirmation: "y"
+    visit signin_path
+    fill_in "Email or Username", with: ""
+    fill_in "Password", with: "y"
+    click_button "Sign In"
+    e(current_path).to eq session_path
+    e(page).to have_text "Error"
+  end
+
+  it "authenticates with a good username, redirects, & flashes an msg" do
+    u = User.create user_attributes username: "x", password: "y", password_confirmation: "y"
+    visit signin_path
+    fill_in "Email or Username", with: "x"
+    fill_in "Password", with: "y"
+    click_button "Sign In"
+    e(current_path).to eq user_path(u)
+    e(page).to have_text "Welcome back"
+  end
+
+  it "authenticates with a good email, redirects, & flashes an msg" do
+    u = User.create user_attributes email: "x@x", password: "y", password_confirmation: "y"
+    visit signin_path
+    fill_in "Email or Username", with: "x@x"
+    fill_in "Password", with: "y"
+    click_button "Sign In"
+    e(current_path).to eq user_path(u)
+    e(page).to have_text "Welcome back"
   end
 
 end
